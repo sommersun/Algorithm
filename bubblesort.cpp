af1a87a1887334f507a93778ee92b7f80b549496
sommersun <50041042+sommersun@users.noreply.github.com>
2022-01-05 17:53:13 +0800

    Update bubblesort.cpp

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -1,1 +1,29 @@
+#include <iostream>
 #include <vector>
+class Sort{
+public:
+	void bubbleSort(std::vector<int>& nums);
+};
+
+void Sort::bubbleSort(std::vector<int>& nums) {
+	int n = nums.size();
+	//sort from small to big
+	for (int i = 0; i < n - 1; i++) {//scan n-1
+		for (int j = 0; j < n - i - 1; j++) {
+			if (nums[j] > nums[j + 1]) {
+				std::swap(nums[j], nums[j + 1]);
+			}
+		}
+	}
+}
+
+int main()
+{
+	std::vector<int> nums = { 5,8,3,9,1 };
+	Sort mysort;
+	mysort.bubbleSort(nums);
+	for (int i = 0; i < nums.size(); i++) {
+		std::cout << nums[i] << " ";
+	}
+}
+
