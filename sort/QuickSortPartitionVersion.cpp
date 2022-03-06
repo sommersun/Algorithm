d00b7e43b21e27323632129a66b03f8b1c390feb
sommersun <50041042+sommersun@users.noreply.github.com>
2022-03-06 23:41:21 +0800

    Create QuickSortPartitionVersion.cpp
    tired march night, maybe meaningless coding

---
+++ sort/QuickSortPartitionVersion.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+class Sort {
+private:
+	int Partition(std::vector<int>& nums,int left,int right);
+public:
+	void QuickSort(std::vector<int>& nums, int left, int right);
+};
+int Sort::Partition(std::vector<int>& nums,int left,int right) {
+	int temp = nums[left];
+	while (left < right) {
+		while (right > left && nums[right] >= temp) right--;
+		nums[left] = nums[right];
+		while (right > left && nums[left] <= temp) left++;
+		nums[right] = nums[left];
+	}
+	nums[right] = temp;
+	return left;
+}
+void Sort::QuickSort(std::vector<int>& nums, int left, int right)
+{
+	if (left >= right) {
+		return;
+	}
+	int pivot = Partition(nums, left, right);
+	QuickSort(nums, left, pivot-1);
+	QuickSort(nums, pivot+1, right);
+
+}
+int main()
+{
+	std::vector<int> nums = { 5,8,3,9,1 };
+	Sort mysort;
+	mysort.QuickSort(nums, 0, 4);
+	for (int i = 0; i < nums.size(); i++) {
+		std::cout << nums[i] << " ";
+	}
+}
